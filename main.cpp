1ba161fd3fa70272f351a5dd970be3c7cd651b41
adro <alu0101349480@ull.edu.es>
2022-11-09 02:29:17 +0000

    Lazy Foo's L5

--- main.cpp
+++ main.cpp
@@ -1,68 +1,98 @@
 #include <iostream>
+#include <random>
 #include <SDL.h>
-#include "include/sdl_functions_own.h"
+#include <algorithm>
+#include "include/world.h"
 
-SDL_Window *globals::window = nullptr;
-SDL_Surface *globals::screenSurface = nullptr;
-SDL_Texture *globals::gTexture = nullptr;
-SDL_Renderer *globals::renderer = nullptr;
+#define WORLDSIZE_X 100
+#define WORLDSIZE_Y 100
+
+void draw_frame(SDL_Renderer *renderer) {
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderDrawLine(renderer, 0, WORLDSIZE_Y - 1, WORLDSIZE_X, WORLDSIZE_Y - 1);
+  SDL_RenderDrawLine(renderer, 0, 0, 0, WORLDSIZE_Y);
+  SDL_RenderDrawLine(renderer, WORLDSIZE_X - 1, 0, WORLDSIZE_X - 1, WORLDSIZE_Y);
+  SDL_RenderDrawLine(renderer, 0, 0, WORLDSIZE_X, 0);
+}
+
+void draw_world(world &w, SDL_Renderer *renderer) {
+
+  for (int i = w.get_player_pos().first - 1; i < w.get_player_pos().first + 1; ++i) {
+    for (int j = w.get_player_pos().second - 1; j < w.get_player_pos().second + 1; ++j) {
+      if (w.get_matriz()[i][j].get_state() == 'O') {
+        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255); // yellow
+        SDL_RenderDrawPoint(renderer, i, j);
+      }
+    }
+  }
+
+  SDL_SetRenderDrawColor(renderer, rand() + 1, rand() + 1, rand() + 1, 255);
+  SDL_RenderDrawPoint(renderer, w.get_end_pos().first, w.get_end_pos().second);
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_RenderDrawPoint(renderer, w.get_player_pos().first, w.get_player_pos().second);
+  SDL_RenderPresent(renderer);
+
+}
 
 int main(int argc, char *argv[]) {
 
-  if (!init()) {
-    std::cout << "Fallo la inicialización" << std::endl;
-  } else {
-    if (!loadMedia()) {
-      std::cout << "Fallo la carga de la imagen" << std::endl;
-    } else {
+  // inicializamos SDL
+  //
 
-      bool quit = false;
-      SDL_Event e;
+  SDL_Window *window = nullptr;
+  SDL_Renderer *renderer = nullptr;
+  window = SDL_CreateWindow("Select sort",
+                            SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED,
+                            100 * 10,
+                            100 * 10,
+                            0);
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  SDL_RenderSetLogicalSize(renderer, WORLDSIZE_X, WORLDSIZE_Y);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
 
-      while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-          if (e.type == SDL_QUIT) {
-            quit = true;
-          }
+
+  // inicializamos el mundo
+  //
+
+  world w(WORLDSIZE_X, WORLDSIZE_Y);
+  w.set_player();
+  w.set_end();
+
+  int timestop = 15;
+  if (WORLDSIZE_X > 500 || WORLDSIZE_Y > 500) {
+    timestop = 5;
+  }
+
+  draw_frame(renderer);
 
 
-          SDL_Rect topLeftViewport;
-          topLeftViewport.x = 0;
-          topLeftViewport.y = 0;
-          topLeftViewport.w = globals::SCREEN_WIDTH / 2;
-          topLeftViewport.h = globals::SCREEN_HEIGHT / 2;
-          SDL_RenderSetViewport( globals::renderer, &topLeftViewport );
-
-          //Render texture to screen
-          SDL_RenderCopy( globals::renderer, globals::gTexture, NULL, NULL );
-
-          SDL_Rect bottomViewport;
-          bottomViewport.x = 0;
-          bottomViewport.y = globals::SCREEN_HEIGHT / 2;
-          bottomViewport.w = globals::SCREEN_WIDTH;
-          bottomViewport.h = globals::SCREEN_HEIGHT / 2;
-          SDL_RenderSetViewport( globals::renderer, &bottomViewport );
-
-          //Render texture to screen
-          SDL_RenderCopy( globals::renderer, globals::gTexture, NULL, NULL );
-
-          // render top right viewport
-          SDL_Rect topRightViewport;
-          topRightViewport.x = globals::SCREEN_WIDTH / 2;
-          topRightViewport.y = 0;
-          topRightViewport.w = globals::SCREEN_WIDTH / 2;
-          topRightViewport.h = globals::SCREEN_HEIGHT / 2;
-
-          SDL_RenderSetViewport( globals::renderer, &topRightViewport );
-          SDL_RenderCopy( globals::renderer, globals::gTexture, NULL, NULL );
-
-          SDL_RenderPresent(globals::renderer);
-
-          }
-        }
+  bool quit = false;
+  SDL_Event event;
+  while (!quit) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        quit = true;
       }
-    close();
     }
+    w.getnexstate();
+    draw_world(w, renderer);
+    SDL_Delay(timestop);
+
+
+    if (w.get_player_pos() == w.get_end_pos()) {
+      quit = true;
+    }
+  }
+
+  std::cout << "PATH FOUND" << std::endl;
+  SDL_Delay(1000);
+
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   return 0;
 }
 
